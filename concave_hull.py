696e0582e2e7a87afe7168b88070b350e238a065
RedDevil91 <b.attila91@gmail.com>
2016-11-29 16:27:35 +0100

    Working implementation

--- concave_hull.py
+++ concave_hull.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial import KDTree, cKDTree
+from scipy.spatial import KDTree, cKDTree, ConvexHull
 from matplotlib.patches import Polygon, Circle
 
 
@@ -17,14 +17,10 @@ class ConcaveHull(object):
         return
 
     def computeVertices(self):
-        i = 0
         while True:
-            if i > 20:
-                break
             dists, idxs = self.findKNearestNeighbour(self.points[self.vertices[-1]])
-
+            idxs = np.delete(idxs, np.where(dists == np.inf))
             next_points, indexes = self.findNextPoint(idxs, self.getBasePoints())
-
             next_point = self.findIntersections(next_points)
 
             if (next_point[0] == self.start_point[0] and
@@ -36,8 +32,6 @@ class ConcaveHull(object):
 
             if len(self.vertices) == 4:
                 self.temp_list = np.append(self.temp_list, [self.start_point], 0)
-
-            i += 1
         return
 
     def findStartPoint(self):
@@ -46,23 +40,20 @@ class ConcaveHull(object):
 
     def findKNearestNeighbour(self, point):
         self.temp_list = self.excludePointFromList(point, self.temp_list)
-        if len(self.temp_list) <= self.k:
-            # TODO: if len <= k then return the remaining points (dist, idxs)
-            pass
         tree = cKDTree(self.temp_list.copy())
         return tree.query(point, k=self.k)
 
     def findNextPoint(self, indexes, base_points):
         base_vector = self.getVectorFromTwoPoint(base_points[0], base_points[1])
         angles = np.array([])
-        points = self.temp_list[indexes]
-        for point in points:
+        candidates = self.temp_list[indexes]
+        for point in candidates:
             vector = self.getVectorFromTwoPoint(base_points[0], point)
             angle = np.arctan2(np.cross(base_vector, vector), np.dot(base_vector, vector))
             angle = angle if angle < 0 else (-np.pi - (np.pi - angle))
             angles = np.append(angles, angle)
         idxs = np.argsort(angles)
-        return points[idxs], indexes[idxs]
+        return candidates[idxs], indexes[idxs]
 
     def findIndexOfPoint(self, point):
         return np.where(np.logical_and(self.points[:, 0] == point[0],
@@ -74,19 +65,19 @@ class ConcaveHull(object):
         return
 
     def findIntersections(self, point_list):
-        if len(self.vertices) < 3:
-            return point_list[0]
-        for point in point_list:
-            curr_segment = [self.points[self.vertices[-1]], point]
-            intersects = False
-            for i in xrange(len(self.vertices) - 1):
-                segment = [self.points[self.vertices[i]], self.points[self.vertices[i + 1]]]
-                if (self.checkSegmentIntersections(curr_segment, segment) and
-                        not self.checkSamePointsInLines(curr_segment, segment)):
-                    intersects = True
-                    break
-            if not intersects:
-                return point
+        if len(self.vertices) >= 3:
+            for point in point_list:
+                curr_segment = [self.points[self.vertices[-1]], point]
+                intersects = False
+                for i in xrange(len(self.vertices) - 1):
+                    segment = [self.points[self.vertices[i]], self.points[self.vertices[i + 1]]]
+                    if (self.checkSegmentIntersections(curr_segment, segment) and
+                            not self.checkSamePointsInLines(curr_segment, segment)):
+                        intersects = True
+                        break
+                if not intersects:
+                    return point
+        return point_list[0]
 
     def getBasePoints(self):
         if len(self.vertices) == 1:
@@ -121,16 +112,17 @@ class ConcaveHull(object):
 
     @staticmethod
     def checkSamePointsInLines(line1, line2):
-        return (line1[0][0] == line2[0][0] or line1[0][0] == line2[1][0] or
-                line1[1][0] == line2[0][0] or line1[1][0] == line2[1][0] or
-                line1[0][1] == line2[0][1] or line1[0][1] == line2[1][1] or
-                line1[1][1] == line2[0][1] or line1[1][1] == line2[1][1])
+        return ((line1[0][0] == line2[0][0] and line1[0][1] == line2[0][1]) or
+                (line1[0][0] == line2[1][0] and line1[0][1] == line2[1][1]) or
+                (line1[1][0] == line2[0][0] and line1[1][1] == line2[0][1]) or
+                (line1[1][0] == line2[1][0] and line1[1][1] == line2[1][1]))
 
 if __name__ == '__main__':
+    import time
     # np.random.seed(3)
-    # np.random.seed(2)
-    np.random.seed(4)
-    points = np.random.random_sample((30, 2))
+    # np.random.seed(2)  # bad result k=6,7
+    # np.random.seed(4)  # internal edges k=6,7
+    points = np.random.random_sample((50, 2))
     # points = np.array([
     #     [7, 1],
     #     [4, 2],
@@ -152,37 +144,52 @@ if __name__ == '__main__':
 
     plt.plot(points[:, 0], points[:, 1], 'o', ms=10, color='blue')
 
-    hull = ConcaveHull(points, k=6)
-    plt.plot(hull.start_point[0], hull.start_point[1], 'o', ms=10, color='red')
+    print "Process time of concave calculation:"
+    start = time.time()
+    concave_hull = ConcaveHull(points, k=6)
+    end = time.time()
+    print end - start
 
-    # dist, idxs = hull.findKNearestNeighbour(hull.start_point)
-    # plt.plot(hull.temp_list[idxs, 0], hull.temp_list[idxs, 1], 'o', ms=10, color='green')
+    print "Process time of convex calculation:"
+    start = time.time()
+    convex_hull = ConvexHull(points)
+    end = time.time()
+    print end - start
+
+    plt.plot(concave_hull.start_point[0], concave_hull.start_point[1], 'o', ms=10, color='red')
+
+    # dist, idxs = concave_hull.findKNearestNeighbour(concave_hull.start_point)
+    # plt.plot(concave_hull.temp_list[idxs, 0], concave_hull.temp_list[idxs, 1], 'o', ms=10, color='green')
     #
     # ax = plt.subplot()
     #
-    # ax.add_patch(Circle(hull.start_point, radius=np.max(dist), fill=False))
+    # ax.add_patch(Circle(concave_hull.start_point, radius=np.max(dist), fill=False))
     #
-    # temp_point = np.array([(hull.start_point[0] - 1), hull.start_point[1]])
-    # next_point, indexes = hull.findNextPoint(idxs, [hull.start_point, temp_point])
+    # temp_point = np.array([(concave_hull.start_point[0] - 1), concave_hull.start_point[1]])
+    # next_point, indexes = concave_hull.findNextPoint(idxs, [concave_hull.start_point, temp_point])
     # plt.plot(next_point[0][0], next_point[0][1], 'o', ms=10, color='yellow')
     #
-    # hull.addPointToVertices(next_point[0])
+    # concave_hull.addPointToVertices(next_point[0])
     #
-    # dist, idxs = hull.findKNearestNeighbour(next_point[0])
-    # plt.plot(hull.temp_list[idxs, 0], hull.temp_list[idxs, 1], 'o', ms=10, color='green')
+    # dist, idxs = concave_hull.findKNearestNeighbour(next_point[0])
+    # plt.plot(concave_hull.temp_list[idxs, 0], concave_hull.temp_list[idxs, 1], 'o', ms=10, color='green')
     #
-    # next_point2, indexes = hull.findNextPoint(idxs, [next_point[0], hull.start_point])
+    # next_point2, indexes = concave_hull.findNextPoint(idxs, [next_point[0], concave_hull.start_point])
     # plt.plot(next_point2[0][0], next_point2[0][1], 'o', ms=10, color='yellow')
     #
-    # hull.addPointToVertices(next_point2[0])
+    # concave_hull.addPointToVertices(next_point2[0])
     #
-    # dist, idxs = hull.findKNearestNeighbour(next_point2[0])
-    # plt.plot(hull.temp_list[idxs, 0], hull.temp_list[idxs, 1], 'o', ms=10, color='green')
+    # dist, idxs = concave_hull.findKNearestNeighbour(next_point2[0])
+    # plt.plot(concave_hull.temp_list[idxs, 0], concave_hull.temp_list[idxs, 1], 'o', ms=10, color='green')
     #
-    # next_point3, indexes = hull.findNextPoint(idxs, [next_point2[0], next_point[0]])
+    # next_point3, indexes = concave_hull.findNextPoint(idxs, [next_point2[0], next_point[0]])
     # plt.plot(next_point3[0][0], next_point3[0][1], 'o', ms=10, color='yellow')
     #
-    # hull.addPointToVertices(next_point3[0])
+    # concave_hull.addPointToVertices(next_point3[0])
 
-    plt.plot(points[hull.vertices, 0], points[hull.vertices, 1], '-')
+    plt.plot(points[concave_hull.vertices, 0], points[concave_hull.vertices, 1], '-')
+
+    ax = plt.subplot()
+    ax.add_patch(Polygon(points[convex_hull.vertices], closed=True, fill=False, color='r'))
+    # plt.plot(points[convex_hull.vertices, 0], points[convex_hull.vertices, 1], '-', color='r')
     plt.show()
